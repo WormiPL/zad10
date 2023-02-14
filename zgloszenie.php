b87b0b33e9f83a47de914a02f8b75ea8ef63b5e5
WormiPL <97028348+WormiPL@users.noreply.github.com>
2023-02-14 13:25:14 +0100

    Add files via upload

--- zgloszenie.php
+++ zgloszenie.php
@@ -1,4 +1,19 @@
 <?php
+$conn=new mysqli("localhost","root", "","wedkarstwo");
 
+if($conn->connect_error){
+    die("error" . $conn->connect_error);
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $lowisko=$_REQUEST["lowisko"];
+    $data=$_REQUEST["data"];
+    $sedzia=$_REQUEST["sedzia"];
+
+    $sql = "INSERT INTO zawody_wedkarskie(Karty_wedkarskie_id, Lowisko_id, data_zawodow, sedzia) VALUES ('0', '$lowisko', '$data', '$sedzia')";
+    $conn->query($sql);
+    $conn->close();
+}
 
 ?>
